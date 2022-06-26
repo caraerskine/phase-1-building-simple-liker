15495bbbcedeeb337a869e6e3a901bb2799129a8
CaraErskine <caraerskine@gmail.com>
2022-06-26 13:55:57 -0400

    commit tree c49e1ecaac446bb8926c607f23601a2c237f3d9f

--- main.js
+++ main.js
@@ -1,10 +1,33 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+//use that for condition
 // Your JavaScript code goes here!
+document.querySelector("#modal").hidden = true
+const heartLiker = document.querySelectorAll(".like-glyph")
 
-
+heartLiker.forEach(heart => {
+  console.log(heart)
+  heart.addEventListener("click", () => {
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+    .then(() => {
+      heart.className = "activated-heart"
+      heart.textContent = FULL_HEART
+    })
+    .catch(() => {
+      document.querySelector("#modal").hidden = false
+      setTimeout(() => {
+        document.querySelector("#modal").hidden = true
+      }, 3000)
+    })
+  } else {
+    heart.textContent = EMPTY_HEART
+    heart.classList.remove("activated-heart")
+  }
+  })
+})
+//First bit- Add the .hidden class to the error modal in the HTML so it does not appear when the page first loads
 
 
 //------------------------------------------------------------------------------
